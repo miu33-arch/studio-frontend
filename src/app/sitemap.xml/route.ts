03a4d069d5e8ebc94e67ed20e0fc76b7bec35314
miu33-arch <padillaanamy83@gmail.com>
2026-09-16 02:02:22 +0300

    fix(sitemap): serve dynamic xml route with explicit application/xml headers

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,41 @@
+export async function GET() {
+  const sitemapXmlString = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://miu33archstudio.xyz/</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://miu33archstudio.xyz/core</loc>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://miu33archstudio.xyz/aio</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://miu33archstudio.xyz/aeo</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://miu33archstudio.xyz/geo</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://miu33archstudio.xyz/sxo</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://miu33archstudio.xyz/api-index</loc>
+    <priority>0.7</priority>
+  </url>
+</urlset>`;
+
+  return new Response(sitemapXmlString, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml; charset=UTF-8",
+      "Cache-Control": "public, max-age=3600, s-maxage=3600",
+    },
+  });
+}
