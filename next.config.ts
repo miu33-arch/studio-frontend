7f36d45ae0711cea4190269cf38ad435765667cc
miu33-arch <padillaanamy83@gmail.com>
2026-09-18 02:13:10 +0300

    fix(csp): allow api.miu33archstudio.xyz in connect-src header

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,8 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-           value: "default-src 'self' https: data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://geo.miu33archstudio.xyz https://*.workers.dev http://127.0.0.1:5000 http://localhost:5000 ws: wss:;",
+            value:
+              "default-src 'self' https: data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://api.miu33archstudio.xyz https://geo.miu33archstudio.xyz https://*.workers.dev http://127.0.0.1:5000 http://localhost:5000 ws: wss:;",
           },
           {
             key: "X-Frame-Options",
